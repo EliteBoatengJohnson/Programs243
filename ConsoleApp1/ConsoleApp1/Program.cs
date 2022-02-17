2c5111ad8123a9067063e081431c74af075f9283
boatengjohnson <80701309+EliteBoatengJohnson@users.noreply.github.com>
2022-02-17 08:50:24 +0000

    new changes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+if(i = 0 ; i < 10){
+    console.WriteLine("David is leaving with his Naked Ass");
+    i++;
+}else{
+    console.WriteLine("He's Staying");
+}
+
