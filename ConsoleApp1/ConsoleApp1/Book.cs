da57c3ac7f7218cf6947c0150863341b43cd510f
boatengjohnson <80701309+EliteBoatengJohnson@users.noreply.github.com>
2022-02-23 23:59:40 +0000

    commit in books

---
+++ ConsoleApp1/ConsoleApp1/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Book
+{
+	
+	public string Title { get; set; }	
+	public List<string> Authors { get; set; }
+
+	public DateTime PublicationDate { get; set; }
+	public string Description { get; set; }	
+}
